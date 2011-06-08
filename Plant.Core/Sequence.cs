e3899435ff31b573b2f26670a07d1b9098348a83
Jeff Deville <jeffdeville@gmail.com>
2011-06-08 15:45:30 -0400

    added the Sequence.cs file

---
+++ Plant.Core/Sequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plant.Core
+{
+    public interface ISequence
+    {
+        Delegate Func { get; }
+    }
+    public class Sequence<TResult> : ISequence
+    {
+        public Sequence(Func<int, TResult> func)
+        {
+            Func = func;
+        }
+
+        public Delegate Func { get; private set; }
+    }
+}
